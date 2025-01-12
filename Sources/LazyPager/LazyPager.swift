046863d3729ecc97783ccefd587e8d40729629ec
Kyle Giammarco <kyle@giammar.co>
2025-01-12 01:50:44 -0500

    add right to left support

--- Sources/LazyPager/LazyPager.swift
+++ Sources/LazyPager/LazyPager.swift
@@ -6,8 +6,8 @@
 //
 
 import Foundation
-import UIKit
 import SwiftUI
+import UIKit
 
 public enum LoadMore {
     case lastElement(minus: Int = 0)
@@ -23,6 +23,11 @@ public enum Direction {
     case vertical
 }
 
+public enum Orientation {
+    case leftToRight
+    case rightToLeft
+}
+
 public enum ListPosition {
     case beginning
     case end
@@ -36,13 +41,13 @@ public enum ZoomConfig {
 public struct Config<Element> {
     /// binding variable to control a custom background opacity. LazyPager is transparent by default
     public var backgroundOpacity: Binding<CGFloat>?
-    
+
     /// Called when the view is done dismissing - dismiss gesture is disabled if nil
-    public var dismissCallback: (() -> ())?
-    
+    public var dismissCallback: (() -> Void)?
+
     /// Called when tapping once
-    public var tapCallback: (() -> ())?
-    
+    public var tapCallback: (() -> Void)?
+
     /// Called when tapping twice
     public var doubleTapCallback: (() -> ())?
     
@@ -51,82 +56,96 @@ public struct Config<Element> {
     
     /// The offset used to trigger load loadMoreCallback
     public var loadMoreOn: LoadMore = .lastElement(minus: 3)
-    
+
     /// Called when more content should be loaded
-    public var loadMoreCallback: (() -> ())?
-    
+    public var loadMoreCallback: (() -> Void)?
+
     /// Direction of the pager
-    public var direction : Direction = .horizontal
-    
+    public var direction: Direction = .horizontal
+
+    /// Orientation of the pager
+    public var orientation: Orientation = .leftToRight
+
+    /// Whether to snap to page on scroll finished
+    public var snapPages: Bool = true
+
     /// Called whent the end of data is reached and the user tries to swipe again
-    public var overscrollCallback: ((ListPosition) -> ())?
-    
+    public var overscrollCallback: ((ListPosition) -> Void)?
+
     /// The element index + the offset while paging
     public var absoluteContentPosition: Binding<CGFloat>?
-    
+
     /// Called every view update to get the zoom config
     public var zoomConfigGetter: (Element) -> ZoomConfig = { _ in .disabled }
-    
+
     /// Called while zooming to provide the current zoom level for an element
-    public var onZoomHandler: ((Element, CGFloat) -> ())?
+    public var onZoomHandler: ((Element, CGFloat) -> Void)?
 
     /// Advanced settings (only accessibleevia .settings)
-    
+
     /// How may out of view pages to load in advance (forward and backwards)
     public var preloadAmount: Int = 3
-    
+
     /// Minimum swipe velocity needed to trigger a dismiss
     public var dismissVelocity: CGFloat = 1.3
-    
+
     /// the minimum % (between 0 and 1) you need to drag to trigger a dismiss
     public var dismissTriggerOffset: CGFloat = 0.1
-    
+
     /// How long to animate the dismiss once done dragging
     public var dismissAnimationLength: CGFloat = 0.2
-    
+
     /// Cancel SwiftUI animations. Default to true because the dismiss gesture is already animated.
     /// Stacking animations can cause undesirable behavior
     public var shouldCancelSwiftUIAnimationsOnDismiss = true
-    
+
     /// At what drag % (between 0 and 1) the background should be fully transparent
     public var fullFadeOnDragAt: CGFloat = 0.2
-    
+
     /// The minimum scroll distance the in which the pinch gesture is enabled
     public var pinchGestureEnableOffset: Double = 10
-    
+
     /// % ammount (from 0-1) of overscroll needed to call overscrollCallback
     public var overscrollThreshold: Double = 0.15
 }
 
-public struct LazyPager<Element, DataCollecton: RandomAccessCollection, Content: View> where DataCollecton.Index == Int, DataCollecton.Element == Element {
+public struct LazyPager<Element, DataCollecton: RandomAccessCollection, Content: View>
+where DataCollecton.Index == Int, DataCollecton.Element == Element {
     private var viewLoader: (Element) -> Content
     private var data: DataCollecton
-    
+
     @State private var defaultPageInternal = 0
     private var providedPage: Binding<Int>?
-    
+
     private var page: Binding<Int> {
-        providedPage ?? Binding(
-            get: { defaultPageInternal },
-            set: { defaultPageInternal = $0 }
-        )
+        providedPage
+            ?? Binding(
+                get: { defaultPageInternal },
+                set: { defaultPageInternal = $0 }
+            )
     }
-    
+
     var config = Config<Element>()
-    
-    public init(data: DataCollecton,
-                page: Binding<Int>? = nil,
-                direction: Direction = .horizontal,
-                @ViewBuilder content: @escaping (Element) -> Content)  {
+
+    public init(
+        data: DataCollecton,
+        page: Binding<Int>? = nil,
+        direction: Direction = .horizontal,
+        orientation : Orientation = .leftToRight,
+        snapPages: Bool = true,
+        @ViewBuilder content: @escaping (Element) -> Content
+    ) {
         self.data = data
         self.providedPage = page
         self.viewLoader = content
         self.config.direction = direction
+        self.config.snapPages = snapPages
+        self.config.orientation = orientation
     }
 }
 
-public extension LazyPager {
-    func onDismiss(backgroundOpacity: Binding<CGFloat>? = nil, _ callback: @escaping () -> ()) -> LazyPager {
+extension LazyPager {
+    public func onDismiss(backgroundOpacity: Binding<CGFloat>? = nil, _ callback: @escaping () -> Void) -> LazyPager {
         guard config.direction == .horizontal else {
             return self
         }
@@ -135,14 +154,14 @@ public extension LazyPager {
         this.config.dismissCallback = callback
         return this
     }
-    
-    func onTap(_ callback: @escaping () -> ()) -> LazyPager {
+
+    public func onTap(_ callback: @escaping () -> Void) -> LazyPager {
         var this = self
         this.config.tapCallback = callback
         return this
     }
-    
-    func onDoubleTap(_ callback: @escaping () -> ()) -> LazyPager {
+
+    public func onDoubleTap(_ callback: @escaping () -> Void) -> LazyPager {
         var this = self
         this.config.doubleTapCallback = callback
         return this
@@ -160,34 +179,34 @@ public extension LazyPager {
         this.config.loadMoreCallback = callback
         return this
     }
-    
-    func zoomable(min: CGFloat, max: CGFloat, doubleTapGesture: DoubleTap = .scale(0.5)) -> LazyPager {
+
+    public func zoomable(min: CGFloat, max: CGFloat, doubleTapGesture: DoubleTap = .scale(0.5)) -> LazyPager {
         var this = self
         this.config.zoomConfigGetter = { _ in
             return .custom(min: min, max: max, doubleTap: doubleTapGesture)
         }
         return this
     }
-    
-    func zoomable(onElement: @escaping (Element) -> ZoomConfig) -> LazyPager {
+
+    public func zoomable(onElement: @escaping (Element) -> ZoomConfig) -> LazyPager {
         var this = self
         this.config.zoomConfigGetter = onElement
         return this
     }
-    
-    func settings(_ adjust: @escaping (inout Config<Element>) -> ()) -> LazyPager {
+
+    public func settings(_ adjust: @escaping (inout Config<Element>) -> Void) -> LazyPager {
         var this = self
         adjust(&this.config)
         return this
     }
-    
-    func overscroll(_ callback: @escaping (ListPosition) -> ()) -> LazyPager {
+
+    public func overscroll(_ callback: @escaping (ListPosition) -> Void) -> LazyPager {
         var this = self
         this.config.overscrollCallback = callback
         return this
     }
-    
-    func absoluteContentPosition(_ absoluteContentPosition: Binding<CGFloat>? = nil) -> LazyPager {
+
+    public func absoluteContentPosition(_ absoluteContentPosition: Binding<CGFloat>? = nil) -> LazyPager {
         guard config.direction == .horizontal else {
             return self
         }
@@ -195,8 +214,8 @@ public extension LazyPager {
         this.config.absoluteContentPosition = absoluteContentPosition
         return this
     }
-    
-    func onZoom(_ onZoomHandler: @escaping (Element, CGFloat) -> ()) -> LazyPager {
+
+    public func onZoom(_ onZoomHandler: @escaping (Element, CGFloat) -> Void) -> LazyPager {
         var this = self
         this.config.onZoomHandler = onZoomHandler
         return this
@@ -223,7 +242,7 @@ extension LazyPager: UIViewControllerRepresentable {
             // Index was explicitly updated
             uiViewController.goToPage(page.wrappedValue, animated: context.transaction.animation != nil)
         }
-        
+
         if page.wrappedValue >= data.count {
             uiViewController.goToPage(data.count - 1, animated: false)
         } else if page.wrappedValue < 0 {
--- Sources/LazyPager/PagerView.swift
+++ Sources/LazyPager/PagerView.swift
@@ -1,35 +1,36 @@
 //
 //  PagerView.swift
-//  
+//
 //
 //  Created by Brian Floersch on 7/8/23.
 //
 
 import Foundation
-import UIKit
 import SwiftUI
+import UIKit
 
 protocol ViewLoader: AnyObject {
-    
+
     associatedtype Element
     associatedtype Content: View
-    
+
     var dataCount: Int { get }
-    
+
     func loadView(at: Int) -> ZoomableView<Element, Content>?
     func updateHostedView(for zoomableView: ZoomableView<Element, Content>)
 }
 
-class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScrollViewDelegate where Loader.Element == Element, Loader.Content == Content {
-    
+class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScrollViewDelegate
+where Loader.Element == Element, Loader.Content == Content {
+
     var isFirstLoad = false
     var loadedViews = [ZoomableView<Element, Content>]()
     var config: Config<Element>
     weak var viewLoader: Loader?
-    
+
     var isRotating = false
     var page: Binding<Int>
-    
+
     var currentIndex: Int = 0 {
         didSet {
             computeViewState()
@@ -63,19 +64,21 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
         self.page = page
         self.config = config
         super.init(frame: .zero)
-        
+
         showsVerticalScrollIndicator = false
         showsHorizontalScrollIndicator = false
         backgroundColor = .clear
-        isPagingEnabled = true
+        isPagingEnabled = config.snapPages
+        if config.orientation == .rightToLeft {
+            self.transform = CGAffineTransformMakeRotation(.pi);
+        }
         delegate = self
     }
-    
-    
+
     required init?(coder: NSCoder) {
         fatalError("Not implemented")
     }
-    
+
     public override func layoutSubviews() {
         super.layoutSubviews()
         if !isFirstLoad {
@@ -85,13 +88,13 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             ensureCurrentPage(animated: false)
         }
     }
-    
+
     func computeViewState() {
         delegate = nil
         DispatchQueue.main.async {
             self.delegate = self
         }
-        
+
         if subviews.isEmpty {
             for i in currentIndex...(currentIndex + config.preloadAmount) {
                 appendView(at: i)
@@ -100,7 +103,7 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
                 prependView(at: i)
             }
         }
-        
+
         if let lastView = loadedViews.last {
             let diff = lastView.index - currentIndex
             if diff < (config.preloadAmount) {
@@ -109,7 +112,7 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
                 }
             }
         }
-        
+
         if let firstView = loadedViews.first {
             let diff = currentIndex - firstView.index
             if diff < (config.preloadAmount) {
@@ -119,20 +122,22 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             }
         }
         self.removeOutOfFrameViews()
-        
+
         // Debug
-//         print(self.loadedViews.map { $0.index })
+        //         print(self.loadedViews.map { $0.index })
     }
-    
-    
+
     func addSubview(_ zoomView: ZoomableView<Element, Content>) {
+        if config.orientation == .rightToLeft {
+            zoomView.transform = CGAffineTransformMakeRotation(.pi);
+        }
         super.addSubview(zoomView)
         NSLayoutConstraint.activate([
             zoomView.widthAnchor.constraint(equalTo: frameLayoutGuide.widthAnchor),
             zoomView.heightAnchor.constraint(equalTo: frameLayoutGuide.heightAnchor),
         ])
     }
-    
+
     func addFirstView(_ zoomView: ZoomableView<Element, Content>) {
         if config.direction == .horizontal {
             zoomView.leadingConstraint = zoomView.leadingAnchor.constraint(equalTo: leadingAnchor)
@@ -146,19 +151,19 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             zoomView.bottomConstraint?.isActive = true
 
         }
-        
+
     }
-    
+
     func appendView(at index: Int) {
         guard let zoomView = viewLoader?.loadView(at: index) else { return }
-        
+
         addSubview(zoomView)
-        
+
         if let lastView = loadedViews.last {
             if config.direction == .horizontal {
                 lastView.trailingConstraint?.isActive = false
                 lastView.trailingConstraint = nil
-                
+
                 zoomView.leadingConstraint = zoomView.leadingAnchor.constraint(equalTo: lastView.trailingAnchor)
                 zoomView.trailingConstraint = zoomView.trailingAnchor.constraint(equalTo: trailingAnchor)
                 zoomView.leadingConstraint?.isActive = true
@@ -166,29 +171,29 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             } else {
                 lastView.bottomConstraint?.isActive = false
                 lastView.bottomConstraint = nil
-                
+
                 zoomView.topConstraint = zoomView.topAnchor.constraint(equalTo: lastView.bottomAnchor)
                 zoomView.bottomConstraint = zoomView.bottomAnchor.constraint(equalTo: bottomAnchor)
                 zoomView.topConstraint?.isActive = true
                 zoomView.bottomConstraint?.isActive = true
             }
-            
+
         } else {
             addFirstView(zoomView)
         }
         loadedViews.append(zoomView)
     }
-    
+
     func prependView(at index: Int) {
         guard let zoomView = viewLoader?.loadView(at: index) else { return }
-        
+
         addSubview(zoomView)
-        
+
         if let firstView = loadedViews.first {
             if config.direction == .horizontal {
                 firstView.leadingConstraint?.isActive = false
                 firstView.leadingConstraint = nil
-                
+
                 zoomView.leadingConstraint = zoomView.leadingAnchor.constraint(equalTo: leadingAnchor)
                 zoomView.trailingConstraint = zoomView.trailingAnchor.constraint(equalTo: firstView.leadingAnchor)
                 zoomView.leadingConstraint?.isActive = true
@@ -196,17 +201,19 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             } else {
                 firstView.topConstraint?.isActive = false
                 firstView.topConstraint = nil
-                
+
                 zoomView.topConstraint = zoomView.topAnchor.constraint(equalTo: topAnchor)
                 zoomView.bottomConstraint = zoomView.bottomAnchor.constraint(equalTo: firstView.topAnchor)
                 zoomView.topConstraint?.isActive = true
                 zoomView.bottomConstraint?.isActive = true
             }
-            
+
         } else {
             addFirstView(zoomView)
         }
-        
+
+        layoutSubviews()
+
         loadedViews.insert(zoomView, at: 0)
         if config.direction == .horizontal {
             contentOffset.x += frame.size.width
@@ -214,26 +221,26 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             contentOffset.y += frame.size.height
         }
     }
-    
+
     func reloadViews() {
         for view in loadedViews {
             viewLoader?.updateHostedView(for: view)
         }
     }
-    
+
     func remove(view: ZoomableView<Element, Content>) {
         let index = view.index
         loadedViews.removeAll { $0.index == view.index }
         view.removeFromSuperview()
-        
+
         if let firstView = loadedViews.first {
-            
+
             if config.direction == .horizontal {
                 firstView.leadingConstraint?.isActive = false
                 firstView.leadingConstraint = nil
                 firstView.leadingConstraint = firstView.leadingAnchor.constraint(equalTo: leadingAnchor)
                 firstView.leadingConstraint?.isActive = true
-                
+
                 if firstView.index > index {
                     contentOffset.x -= frame.size.width
                 }
@@ -242,13 +249,13 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
                 firstView.topConstraint = nil
                 firstView.topConstraint = firstView.topAnchor.constraint(equalTo: topAnchor)
                 firstView.topConstraint?.isActive = true
-                
+
                 if firstView.index > index {
                     contentOffset.y -= frame.size.height
                 }
             }
         }
-        
+
         if let lastView = loadedViews.last {
             if config.direction == .horizontal {
                 lastView.trailingConstraint?.isActive = false
@@ -263,18 +270,17 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             }
         }
     }
-    
-    
+
     func removeOutOfFrameViews() {
         guard let viewLoader = viewLoader else { return }
-        
+
         for view in loadedViews {
             if abs(currentIndex - view.index) > config.preloadAmount || view.index >= viewLoader.dataCount {
                 remove(view: view)
             }
         }
     }
-    
+
     func resizeOutOfBoundsViews() {
         for v in loadedViews {
             if v.index != currentIndex {
@@ -282,14 +288,14 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             }
         }
     }
-    
+
     func goToPage(_ page: Int, animated: Bool) {
         currentIndex = page
         DispatchQueue.main.async {
             self.ensureCurrentPage(animated: animated)
         }
     }
-    
+
     func ensureCurrentPage(animated: Bool) {
         guard let index = loadedViews.firstIndex(where: { $0.index == currentIndex }) else { return }
         if config.direction == .horizontal {
@@ -298,21 +304,21 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
             setContentOffset(CGPoint(x: contentOffset.x, y: CGFloat(index) * frame.size.height), animated: animated)
         }
     }
-    
+
     func loadMoreIfNeeded() {
         guard let loadMoreCallback = config.loadMoreCallback else { return }
         guard case let .lastElement(offset) = config.loadMoreOn else { return }
         guard let viewLoader = viewLoader else { return }
-        
+
         if currentIndex + offset >= viewLoader.dataCount - 1 {
             DispatchQueue.main.async {
                 loadMoreCallback()
             }
         }
     }
-    
+
     // MARK: UISCrollVieDelegate methods
-    
+
     var lastPos: CGFloat = 0
     var hasNotfiedOverscroll = false
     var scrollSettled = true
@@ -322,36 +328,50 @@ class PagerView<Element, Loader: ViewLoader, Content: View>: UIScrollView, UIScr
     }
 
     public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if !scrollView.isTracking, !isRotating, (currentView.index != page.wrappedValue || page.wrappedValue != currentIndex ) {
-            currentIndex = currentView.index
+        var relativeIndex: Int
+        var absoluteOffset: CGFloat
+        if config.direction == .horizontal {
+            absoluteOffset = scrollView.contentOffset.x / scrollView.frame.width
+            relativeIndex = Int(round(absoluteOffset))
+        } else {
+            absoluteOffset = scrollView.contentOffset.y / scrollView.frame.height
+            relativeIndex = Int(round(absoluteOffset))
+        }
+        relativeIndex = relativeIndex < 0 ? 0 : relativeIndex
+        relativeIndex = relativeIndex >= loadedViews.count ? loadedViews.count - 1 : relativeIndex
+
+        if !scrollView.isTracking, !isRotating {
+            currentIndex = loadedViews[relativeIndex].index
             page.wrappedValue = currentIndex
         }
-        
+
         if let index = loadedViews[safe: relativeIndex]?.index {
             config.absoluteContentPosition?.wrappedValue = CGFloat(index) + absoluteOffset - CGFloat(relativeIndex)
         }
-        
+
         if !hasNotfiedOverscroll {
-            if relativeIndex >= loadedViews.count-1, absoluteOffset - CGFloat(relativeIndex) > config.overscrollThreshold {
+            if relativeIndex >= loadedViews.count - 1,
+                absoluteOffset - CGFloat(relativeIndex) > config.overscrollThreshold
+            {
                 config.overscrollCallback?(.end)
                 hasNotfiedOverscroll = true
             }
-            
+
             if relativeIndex <= 0, absoluteOffset - CGFloat(relativeIndex) < -config.overscrollThreshold {
                 config.overscrollCallback?(.beginning)
                 hasNotfiedOverscroll = true
             }
         }
-        
+
         // Horribly janky way to detect when scrolling (both touching and animation) is finnished.
         let caputred: CGFloat
-        
+
         if config.direction == .horizontal {
             caputred = scrollView.contentOffset.x
         } else {
             caputred = scrollView.contentOffset.y
         }
-        
+
         lastPos = caputred
         DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(100)) {
             if self.lastPos == caputred, !scrollView.isTracking {
